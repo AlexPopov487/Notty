8846854e4296434090c64ce1b0b5696aab5b8fdf
apopo <Apopov974@gmail.com>
2021-04-28 23:02:47 +0500

    empty state pic and textviews are added

--- app/src/main/java/com/example/notes/ui/NotesListFragment.kt
+++ app/src/main/java/com/example/notes/ui/NotesListFragment.kt
@@ -1,10 +1,12 @@
 package com.example.notes.ui
 
+import android.opengl.Visibility
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -59,6 +61,7 @@ class NotesListFragment : Fragment() {
 
 
         viewModel.noteData.observe(viewLifecycleOwner, Observer { notesList ->
+            binding.layoutEmptyState.visibility = if (notesList.isEmpty()) View.VISIBLE else View.GONE
             adapter.submitList(notesList)
         })
 
